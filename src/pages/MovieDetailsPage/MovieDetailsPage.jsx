6a83fb6eeabda83d04ebf89d5ce4cef04473a4c9
DSojat <145054801+DSojat@users.noreply.github.com>
2024-05-02 16:13:09 +0300

    fixed

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -5,7 +5,7 @@ import {
   useLocation,
   useNavigate,
 } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { getMovieById } from '../../tmdb-api';
 import css from './MoviesDetails.module.css';
 
@@ -14,8 +14,11 @@ export default function MoviesDetails() {
   const { movieId } = useParams();
   const [error, setError] = useState([false, undefined]);
   const location = useLocation();
+  const valueRef = useRef(location.state);
   const backLinkHref = location.state ?? '/movies';
   const navigate = useNavigate();
+  const defaultImg =
+    'https://dl-media.viber.com/10/share/2/long/vibes/icon/image/0x0/95e0/5688fdffb84ff8bed4240bcf3ec5ac81ce591d9fa9558a3a968c630eaba195e0.jpg';
 
   const handleClick = () => {
     navigate(backLinkHref, { replace: false });
@@ -47,13 +50,15 @@ export default function MoviesDetails() {
             &#8656; Go back
           </button>
           <div className={css.movieMainBox}>
-            {movie.poster_path && (
-              <img
-                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                alt={movie.title}
-                width="250"
-              />
-            )}
+            <img
+              src={
+                movie.poster_path
+                  ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+                  : defaultImg
+              }
+              alt={movie.title}
+              width="250"
+            />
             <div>
               <h1>{movie.title}</h1>
               <p>User score: {Math.ceil(movie.vote_average * 10)}%</p>
@@ -67,10 +72,10 @@ export default function MoviesDetails() {
           </div>
           <div className={css.movieAddBox}>
             <p>Additional information</p>
-            <Link className={css.link} to="cast" state={location.state}>
+            <Link className={css.link} to="cast" state={valueRef.current}>
               Cast
             </Link>
-            <Link className={css.link} to="reviews" state={location.state}>
+            <Link className={css.link} to="reviews" state={valueRef.current}>
               Reviews
             </Link>
           </div>
